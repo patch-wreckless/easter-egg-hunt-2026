47ebfe401a15c376f3ffcfd6088eb8d41e07d3bd
patch-wreckless <234780031+patch-wreckless@users.noreply.github.com>
2026-03-31 18:32:06 -0300

    feat: easter egg hunt

---
+++ README.md
@@ -0,0 +1,87 @@
+# Easter Egg Hunt
+
+## Download The Source
+
+_If you're already familiar with Git and GitHub, download the source using your preferred method._
+
+1. Go to the [project homepage](https://github.com/patch-wreckless/easter-egg-hunt-2026)
+2. Click `<> Code` then `Download ZIP`
+3. Extract the `easter-egg-hunt-2026-main.zip` file
+
+## Create Your Content
+
+Create and update the following files to define the intro message, clues, and success messages:
+
+**content/images/\***
+
+The images referenced by the clues.
+
+**content/clues.json**
+
+This file defines the list of clues.
+
+Each clue is a piece of text that looks like this:
+
+```json
+{
+    "heading": "<the text to show>",
+    "image": "<the name of the image file to show>",
+    "password": "<the password to unlock the next clue>"
+}
+```
+
+The file is a list of clues, separated by commas, and surrounded by `[` and `]` characters. Here's an example of a complete file:
+
+```json
+[
+    {
+        "heading": "spring clue",
+        "image": "spring.jpg",
+        "password": "spring"
+    },
+    {
+        "heading": "jelly beans clue",
+        "image": "jelly-beans.jpg",
+        "password": "jelly beans"
+    },
+    {
+        "heading": "basket clue",
+        "image": "basket.jpg",
+        "password": "basket"
+    }
+]
+```
+
+**content/code.txt**
+
+A text file containing the Easter basket unlock code.
+
+**content/intro.txt**
+
+A text file the welcome message and instructions to show when the website loads.
+
+**content/success.json**
+
+This file defines the messages the player sees when they solve all the clues.
+
+```json
+{
+    "successMessage": "You did it!",
+    "finalClueLabel": "Easter basket unlock code",
+    "finalClueValue": "1234"
+}
+```
+
+## A Web Server
+
+_If you're already familiar with web servers, use which ever server you like._
+
+Web browsers will open HTML files directly from your computer but generally don't like to load the other files they reference so you'll need to run a web server to serve this site.
+
+1. Download and install [`python3`](https://www.python.org/downloads/)
+2. Open a terminal
+    - `Windows Powershell` or `Command Prompt` on Windows
+3. In the terminal, navigate to the `www` folder in the extracted source folder
+    - e.g. `cd ~/Downloads/easter-egg-hunt-2026-main/www`
+4. In the terminal, run `python3 -m http.server 8000`
+5. Open http://localhost:8000/ in your browser
---
+++ www/script.js
@@ -0,0 +1,101 @@
+const loading = document.getElementById("loading");
+
+const intro = document.getElementById("intro");
+const introText = document.getElementById("intro-text");
+const startBtn = document.getElementById("start-btn");
+
+const game = document.getElementById("game");
+const clueHeading = document.getElementById("clue-heading");
+const clueImage = document.getElementById("clue-image");
+const passwordInput = document.getElementById("password-input");
+const submitBtn = document.getElementById("submit-btn");
+
+const congratulations = document.getElementById("congratulations");
+const successText = document.getElementById("success-text");
+const finalClueLabel = document.getElementById("final-clue-label");
+const finalClueValue = document.getElementById("final-clue-value");
+
+let index = 0;
+let clues = [];
+
+function showIntro() {
+    intro.classList.remove("hidden");
+    game.classList.add("hidden");
+    congratulations.classList.add("hidden");
+}
+
+function showGame() {
+    intro.classList.add("hidden");
+    game.classList.remove("hidden");
+    congratulations.classList.add("hidden");
+}
+
+function showCongratulations() {
+    intro.classList.add("hidden");
+    game.classList.add("hidden");
+    congratulations.classList.remove("hidden");
+}
+
+function renderClue() {
+    const clue = clues[index];
+    clueHeading.innerText = clue.heading;
+    clueImage.src = "content/" + clue.image;
+    passwordInput.value = "";
+    // todo: remove
+    passwordInput.value = clue.password.toLowerCase();
+    passwordInput.classList.remove("error");
+}
+
+function markWrong() {
+    passwordInput.classList.add("error");
+    submitBtn.classList.add("shake");
+
+    setTimeout(() => {
+        submitBtn.classList.remove("shake");
+    }, 250);
+}
+
+startBtn.addEventListener("click", () => {
+    showGame();
+    renderClue();
+});
+
+passwordInput.addEventListener("input", () => {
+    passwordInput.classList.remove("error");
+});
+
+submitBtn.addEventListener("click", () => {
+    const guess = passwordInput.value.trim().toLowerCase();
+    const correct = clues[index].password.toLowerCase();
+
+    if (guess !== correct) {
+        markWrong();
+        return;
+    }
+
+    index++;
+    if (index < clues.length) {
+        renderClue();
+        return;
+    }
+
+    showCongratulations();
+});
+
+(async function init() {
+
+    const introRes = await fetch("content/intro.txt");
+    introText.textContent = await introRes.text();
+
+    const cluesRes = await fetch("content/clues.json");
+    clues = await cluesRes.json();
+
+    const successRes = await fetch("content/success.json");
+    const success = await successRes.json();
+    successText.textContent = success.successMessage;
+    finalClueLabel.textContent = success.finalClueLabel;
+    finalClueValue.textContent = success.finalClueValue;
+
+    loading.classList.add("hidden");
+    showIntro();
+})();
